5ee45be154352dfbdab9571ac766afc1e5f50a5d
Alex Seubert <alexseubert@gmail.com>
2013-11-26 11:58:55 -0600

    Get rid of old stuff

--- app/js/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('testApp', []);
-
-app.config(['$routeProvider', function($routeProvider) {
-	$routeProvider.
-	when('/', {templateUrl: 'partials/home.html', controller: homeCtrl})
-	.when('/collections', {templateUrl: 'partials/collections.html', controller: colCtrl})
-	.when('/queries', {templateUrl: 'partials/queries.html', controller: queryCtrl})
-	.otherwise({redirectTo: '/'});
-}]);
