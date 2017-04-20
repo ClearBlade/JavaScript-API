dacc2d19b9098a103ac51f5e4008aef4b25127a1
Sanket Deshpande <ssd20072@gmail.com>
2017-04-20 13:33:08 -0500

    added message history test

--- test/MessageSpec.js
+++ test/MessageSpec.js
@@ -1,6 +1,62 @@
-describe("ClearBlade Messaging", function () {
-  it("connects to MQTT broker", function () {
-    var user = cb.Messaging({}, function(err, data) {
-      expect(err).toBeNull();
-    });
+var msg;
+beforeEach(function() {
+  var originalConstructor = Paho.MQTT.Client;
+  var spyObj;
+  spyOn(Paho.MQTT, "Client").and.callFake(function () {
+    return {
+      connect: function() {}
+    }
   });
+  var cb = new ClearBlade();
+  cb.user = {};
+  cb.user.authToken = "testUserToken";
+  msg = cb.Messaging({}, function(data) {});
+});
+
+
+describe("get message history test", function() {
+  it("calls get message history", function() {
+    var callNum = ClearBlade.request.calls.count();
+    msg.getMessageHistory("topic", 1000, 10, function() {});
+    var expectedData = {
+      method: 'GET',
+      endpoint: 'api/v/1/message/fakeSystemKey',
+      qs: 'topic=topic&count=10&last=1000&start=-1&stop=-1',
+      authToken: 'testUserToken',
+      timeout: undefined,
+      URI: undefined
+    }
+    expect(ClearBlade.request.calls.argsFor(callNum)[0]).toEqual(expectedData);
+  });
+
+  it("calls get message history with timeframe", function() {
+    var callNum = ClearBlade.request.calls.count();
+    msg.getMessageHistoryWithTimeFrame("topic", 25, -1, 12345, 123456, function() {});
+    var expectedData = {
+      method: 'GET',
+      endpoint: 'api/v/1/message/fakeSystemKey',
+      qs: 'topic=topic&count=25&last=-1&start=12345&stop=123456',
+      authToken: 'testUserToken',
+      timeout: undefined,
+      URI: undefined
+    }
+    expect(ClearBlade.request.calls.argsFor(callNum)[0]).toEqual(expectedData);
+  });
+});
+
+
+describe("delete message history test", function() {
+  it("calls get and delete message history", function() {
+    var callNum = ClearBlade.request.calls.count();
+    msg.getAndDeleteMessageHistory("topic", 25, -1, 12345, 123456, function() {});
+    var expectedData = {
+      method: 'DELETE',
+      endpoint: 'api/v/1/message/fakeSystemKey',
+      qs: 'topic=topic&count=25&last=-1&start=12345&stop=123456',
+      authToken: 'testUserToken',
+      timeout: undefined,
+      URI: undefined
+    }
+    expect(ClearBlade.request.calls.argsFor(callNum)[0]).toEqual(expectedData);
+  });
+});
